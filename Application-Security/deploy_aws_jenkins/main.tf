491f2a8b053c30a9bfb4e82d1f19f852918b8f15
Naveed Nawaz <73508041+naveednawazkhan@users.noreply.github.com>
2024-01-03 23:18:34 -0600

    Update main.tf

--- Application-Security/deploy_aws_jenkins/main.tf
+++ Application-Security/deploy_aws_jenkins/main.tf
@@ -112,13 +112,23 @@ resource "aws_instance" "web_instance" {
   sudo apt list --upgradable
   sudo apt install wget -y
   sudo apt install git -y
-  sudo apt install java-11-openjdk.x86_64 -y
-  sudo yum install epel-release -y
-  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
-  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-  sudo yum install jenkins -y
+  sudo apt-get install ca-certificates curl gnupg
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install jenkins -y
   sudo service jenkins start
   sudo systemctl enable jenkins
   sudo systemctl start docker
@@ -131,6 +141,7 @@ resource "aws_instance" "web_instance" {
 
   ## Unlock Jenkins
   ## sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
 
   tags = {
     "Name" : "${var.stack_name}"
